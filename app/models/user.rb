1389d9935332e89ae78d33dcde56345fe64c852f
Karim Tarek <karimmtarek@gmail.com>
2015-02-26 23:49:33 -0500

    tiny code refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,8 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.access_key         = auth.extra.access_token.params[:oauth_token]
-      user.access_secret      = auth.extra.access_token.params[:oauth_token_secret]
+      user.access_key         = auth.extra.access_token.token
+      user.access_secret      = auth.extra.access_token.params.secret
       user.email              = auth.info.email
       user.password           = Devise.friendly_token[0,20]
       user.username           = auth.info.name
