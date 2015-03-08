de7b9575f0c752b89557bf09c01a62ee0315097a
Karim Tarek <karimmtarek@gmail.com>
2015-03-07 19:13:53 -0500

    projects style in popop

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -26,7 +26,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :resize_to_fit => [500, 400]
+  process :resize_to_fill => [500, 350]
   #
   # def scale(width, height)
   #   # do something
@@ -34,7 +34,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [50, 50]
+    process :resize_to_fill => [100, 100]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
