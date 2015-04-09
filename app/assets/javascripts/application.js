9af03089a35d60a2f0a8c2c79728e561c3bfaab7
Karim Tarek <karimmtarek@gmail.com>
2015-04-09 09:42:24 -0400

    assets clean-up

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,5 @@
 //= require jquery
 //= require jquery_ujs
-//= require ../plugins/jquery-ui/jquery-ui-1.10.1.custom.min
-//= require ../plugins/boostrapv3/js/bootstrap.min
-//= require ../plugins/breakpoints
-//= require ../plugins/jquery-unveil/jquery.unveil.min
-//= require ../plugins/jquery-block-ui/jqueryblockui
-//= require ../plugins/jquery-scrollbar/jquery.scrollbar.min
-//= require ../plugins/pace/pace.min
-//= require ../plugins/jquery-numberAnimate/jquery.animateNumbers
-
-//= require backend/core
 //= require backend/extra
 
 //= require nested_form_fields
