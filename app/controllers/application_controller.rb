253960f224107a2a5bbd52fdd0a361272085eea6
Karim Tarek <karimmtarek@gmail.com>
2015-07-21 15:56:16 -0400

    Add username to devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   layout :layout_by_resource
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -19,6 +20,12 @@ class ApplicationController < ActionController::Base
       (action_name == 'new' || action_name == 'create')
       'static_pages'
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
   end
 
   private
