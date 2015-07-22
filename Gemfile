d7c5b33aa1fd2a8acaf9346c3d93ecfeddc591f3
Karim Tarek <karimmtarek@gmail.com>
2015-07-22 00:02:42 -0400

    Setup letter_opener for development env

--- Gemfile
+++ Gemfile
@@ -80,6 +80,7 @@ group :development do
   gem 'guard-livereload', '~> 2.4', require: false
   gem 'faker'
   gem 'better_errors'
+  gem 'letter_opener'
 end
 
 group :test do
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,8 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :letter_opener
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -41,5 +43,4 @@ Rails.application.configure do
 
   # needed for device
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
 end
