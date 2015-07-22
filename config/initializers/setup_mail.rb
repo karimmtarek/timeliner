99d00f7b489a38f8c9510f6f7c86f5668451f851
Karim Tarek <karimmtarek@gmail.com>
2015-07-22 00:02:08 -0400

    Add staging to mail setup

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,4 +1,4 @@
-if Rails.env.development? || Rails.env.production?
+if Rails.env.staging? || Rails.env.production?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address:        'smtp.sendgrid.net',
