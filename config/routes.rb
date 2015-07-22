54fa4fa642dcb08f1a9605cea13b8355afb9ead9
Karim Tarek <karimmtarek@gmail.com>
2015-07-21 22:33:31 -0400

    Fix user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,19 @@
 Rails.application.routes.draw do
+  root 'static_pages#index'
 
   devise_for :users
-  resources :projects
-  resources :images
-
-  put '/milestones/import_linkedin_data', to: "milestones#import_linkedin_data", as: :import_linkedin_data
-
-  post '/timelines/:user_id/contact', to: 'timelines#contact', as: 'contact'
-
-  get '/timelines/:user_id/show_overlay/:milestone_id', to: 'timelines#show_overlay', as: 'show_overlay'
-
-  resources :milestones
-
-  root "static_pages#index"
-  get 'static_pages/index'
-
 
   resources :users do
     resources :timelines, only: [:index, :contact]
   end
 
   get '/:user_id' => 'timelines#index', as: 'timeline'
+
+  post '/timelines/:user_id/contact', to: 'timelines#contact', as: 'contact'
+
+  get '/timelines/:user_id/show_overlay/:milestone_id', to: 'timelines#show_overlay', as: 'show_overlay'
+
+  resources :milestones
+  resources :projects
+  resources :images
 end
