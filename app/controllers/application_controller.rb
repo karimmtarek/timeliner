94a408454cbe7b54a79776fbe523eb37eba7d781
Karim Tarek <karimmtarek@gmail.com>
2015-07-26 00:13:27 -0400

    redirect to root after sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for user
-    user_path(user)
+    root_path
   end
 
   def after_sign_out_path_for user
