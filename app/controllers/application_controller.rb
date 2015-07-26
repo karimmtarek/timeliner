1803b6045e2eb6bc8d2996e86cf79083c0a4f64e
Karim Tarek <karimmtarek@gmail.com>
2015-07-26 00:40:33 -0400

    redirect to user page after signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for user
-    root_path
+    user_path(user)
   end
 
   def after_sign_out_path_for user
