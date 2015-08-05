a027717031d917deb74a6b6981f7ce90c74eb119
Karim Tarek <karimmtarek@gmail.com>
2015-08-05 00:03:03 -0400

    WIP

--- spec/features/send_message_spec.rb
+++ spec/features/send_message_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
-describe "Contact form" do
+describe 'Contact form' do
+  let(:user) { FactoryGirl.create(:user) }
+  before { user.confirm! }
+
   it 'send message to a user' do
-
-    user = User.create!(user_attributes)
-
     visit user_timelines_path(user)
 
     expect(current_path).to eq(user_timelines_path(user))
 
     fill_in 'message[email]', with: "milestone@title.com"
+    fill_in 'message[subject]', with: "subject line!"
     fill_in 'message[body]', with: "company name"
 
     click_button 'Send message'
 
     expect(page).to have_text("Thanks for getting in touch, I'll get back to you as soon as possible.")
-
   end
 end
