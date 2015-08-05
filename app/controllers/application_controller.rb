70262a8c8ac49d5b61d4be2953d4557bcd37b222
Karim Tarek <karimmtarek@gmail.com>
2015-08-04 23:07:11 -0400

    add parenthesis

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def after_sign_in_path_for user
+  def after_sign_in_path_for(user)
     user_path(user)
   end
 
-  def after_sign_out_path_for user
+  def after_sign_out_path_for(user)
     root_path
   end
 
