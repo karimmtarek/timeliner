f7361941743eca24d4c984cb45ff468b43bfddac
Karim Tarek <karimmtarek@gmail.com>
2015-08-13 13:42:05 -0400

    change action mailer defualt url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'gotimeliner.com' }
+  config.action_mailer.default_url_options = { host: 'www.gotimeliner.com' }
 end
