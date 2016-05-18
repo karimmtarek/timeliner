a25d55da29570e5be261201382be647e78cc3760
Karim Tarek <karimmtarek@gmail.com>
2016-05-17 20:46:37 -0400

    Add host url env var for staging/prod

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'timelinerapp-staging.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: ENV['HOST_URL'] }
 end
