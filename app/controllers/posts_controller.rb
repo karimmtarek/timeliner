d2247e1899fc884d07eede3ff1fc3410880df2ea
Karim Tarek <karimmtarek@gmail.com>
2016-05-21 17:39:28 -0400

    Add date field to post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -56,6 +56,6 @@ class PostsController < ApplicationController
   # private
 
   def post_params
-    params.require(:post).permit(:title, :body)
+    params.require(:post).permit(:title, :body, :published_on)
   end
 end
